4db44086e70ca45ef191eefd9f2a8400d6e979c6
Your NamePaul <kenpopaul@yahoo.co.uk>
2022-10-24 20:21:15 +0100

    Class to make counter work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function App() {
-  return null
+  return (
+    <div>
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
+    </div>
+  )
 }
 
 export default App;
